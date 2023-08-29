2da72f098cc39bc25534d7b4c162a0e103be6fe7
Wira Setiawan <wirasetiawan29@gmail.com>
2023-08-29 17:52:38 +0700

    update lib call

--- Sources/TinodiosDB/AccountDb.swift
+++ Sources/TinodiosDB/AccountDb.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import SQLite3
+import SQLite
 
 public class StoredAccount {
     var id: Int64
--- Sources/TinodiosDB/BaseDb.swift
+++ Sources/TinodiosDB/BaseDb.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import SQLite3
+import SQLite
 import TinodeSDK
 
 public class BaseDb {
